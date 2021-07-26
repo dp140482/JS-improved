fd8427080663fa3a9d9720767a6cbe733d8069ee
Dmitry <dp_140482@mail.ru>
2021-07-26 08:21:13 +0300

    Подготовка

---
+++ lesson-2/project/js/main.js
@@ -0,0 +1,61 @@
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+        this.render(); //вывод товаров на страницу
+    }
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ];
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
+
+    sumOfPrices() {
+        let sum = 0;
+        this.goods.forEach(product => sum += product.price);
+        return sum;
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+    }
+}
+
+class Cart {
+    constructor() {
+        this._productIDs = [];
+        this._productCount = [];
+    }
+    pushProduct(product) {}
+    dropProduct(id) {}
+    getSumByProduct() {}
+    getSum() {}
+    render() {}
+}
+
+let list = new ProductList();
